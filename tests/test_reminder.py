eca5e8f8923cd122f9f5c813622505b434e6fa2f
Yevhen Kuzmovych <yevhen.kuzmovych@sizmek.com>
2019-01-26 18:40:02 +0100

    Reminder tests

---
+++ tests/test_reminder.py
@@ -0,0 +1,67 @@
+from unittest import TestCase
+
+from reminder import EmailReminder, PopupReminder
+from serializers.reminder_serializer import ReminderSerializer
+
+
+class TestReminder(TestCase):
+    def test_email_reminder(self):
+        reminder = EmailReminder(34)
+        self.assertEqual(reminder.method, 'email')
+        self.assertEqual(reminder.minutes_before_start, 34)
+
+    def test_popup_reminder(self):
+        reminder = PopupReminder(51)
+        self.assertEqual(reminder.method, 'popup')
+        self.assertEqual(reminder.minutes_before_start, 51)
+
+
+class TestReminderSerializer(TestCase):
+    def test_to_json(self):
+        reminder_json = {
+            'method': 'email',
+            'minutes': 55
+        }
+        reminder = EmailReminder(55)
+
+        self.assertDictEqual(ReminderSerializer.to_json(reminder), reminder_json)
+
+        reminder_json = {
+            'method': 'popup',
+            'minutes': 13
+        }
+        reminder = PopupReminder(13)
+
+        self.assertDictEqual(ReminderSerializer.to_json(reminder), reminder_json)
+
+        serializer = ReminderSerializer(reminder)
+        self.assertDictEqual(serializer.get_json(), reminder_json)
+
+    def test_to_object(self):
+        reminder_json = {
+            'method': 'email',
+            'minutes': 55
+        }
+
+        reminder = ReminderSerializer.to_object(reminder_json)
+
+        self.assertIsInstance(reminder, EmailReminder)
+        self.assertEqual(reminder.minutes_before_start, 55)
+
+        reminder_json = {
+            'method': 'popup',
+            'minutes': 33
+        }
+
+        reminder = ReminderSerializer.to_object(reminder_json)
+
+        self.assertIsInstance(reminder, PopupReminder)
+        self.assertEqual(reminder.minutes_before_start, 33)
+
+        with self.assertRaises(ValueError):
+            reminder_json = {
+                'method': 'telegram',
+                'minutes': 33
+            }
+
+            ReminderSerializer.to_object(reminder_json)
