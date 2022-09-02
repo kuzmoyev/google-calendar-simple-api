11785b03302e688cc6ef70ae17da24a6b1715bae
kuzmoyev <ykuzmovych@luc.id>
2022-09-02 21:05:45 +0200

    Add list_calendar_colors

--- gcsa/google_calendar.py
+++ gcsa/google_calendar.py
@@ -481,8 +481,14 @@ class GoogleCalendar:
         return EventSerializer(event_resource).get_object()
 
     def list_event_colors(self):
-        """List allowed event colors for the calendar."""
+        """A global palette of event colors, mapping from the color ID to its definition.
+        An :py:class:`~gcsa.event.Event` may refer to one of these color IDs in its color_id field."""
         return self.service.colors().get().execute()['event']
+
+    def list_calendar_colors(self):
+        """A global palette of calendar colors, mapping from the color ID to its definition.
+        :py:class:`~gcsa.calendar.CalendarListEntry` resource refers to one of these color IDs in its color_id field."""
+        return self.service.colors().get().execute()['calendar']
 
     def __iter__(self) -> Iterator[Event]:
         return iter(self.get_events())
