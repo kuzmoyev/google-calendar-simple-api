0b512f36c01ef63c73ba85eea4136cb8904e29fb
kuzmoyev <ykuzmovych@luc.id>
2022-09-05 19:07:13 +0200

    Support python 3.10 closes #131

--- setup.py
+++ setup.py
@@ -106,6 +106,7 @@ setup(
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
     ],
     cmdclass={
         'upload': UploadCommand,
