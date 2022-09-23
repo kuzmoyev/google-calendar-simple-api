2017ca52f78a2c9683c15c102dcad5e8dd466a1b
kuzmoyev <ykuzmovych@luc.id>
2022-09-23 13:22:25 +0200

    Add colors service tests

--- tests/google_calendar_tests/mock_services/mock_service.py
+++ tests/google_calendar_tests/mock_services/mock_service.py
@@ -1,5 +1,6 @@
 from .mock_calendar_list_requests import MockCalendarListRequests
 from .mock_calendars_requests import MockCalendarsRequests
+from .mock_colors_requests import MockColorsRequests
 from .mock_events_requests import MockEventsRequests
 
 
@@ -10,6 +11,7 @@ class MockService:
         self._events = MockEventsRequests()
         self._calendars = MockCalendarsRequests()
         self._calendar_list = MockCalendarListRequests()
+        self._colors = MockColorsRequests()
 
     def events(self):
         return self._events
@@ -19,3 +21,6 @@ class MockService:
 
     def calendarList(self):
         return self._calendar_list
+
+    def colors(self):
+        return self._colors
