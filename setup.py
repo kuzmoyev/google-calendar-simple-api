9b459bd9b1760f98fb114b5c523cc7dcbc3ae47b
kuzmoyev <ykuzmovych@luc.id>
2023-02-10 13:20:10 +0100

    Add support for python3.11 closes #149

--- setup.py
+++ setup.py
@@ -118,6 +118,7 @@ setup(
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
     ],
     cmdclass={
         'upload': UploadCommand,
